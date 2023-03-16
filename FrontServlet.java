9d76f87ab71a1d29f67de88ca17393833525df11
Octane-XT <zorakotoasimbola@hotmail.com>
2023-03-17 03:31:36 +0600

    sprint2

---
+++ FrontServlet.java
@@ -0,0 +1,38 @@
+package etu1823.framework.servlet;
+
+import javax.servlet.http.*;
+import javax.servlet.*;
+import java.io.*;
+import java.net.http.HttpClient.Redirect;
+import java.util.HashMap;
+
+import utility.*;
+import etu1823.framework.Mapping;
+
+public class FrontServlet extends HttpServlet {
+  HashMap<String,Mapping> MappingUrls;
+  
+  public HashMap<String, Mapping> getMappingUrls() {
+    return MappingUrls;
+  }
+
+  public void setMappingUrls(HashMap<String, Mapping> mappingUrls) {
+    MappingUrls = mappingUrls;
+  }
+  protected void processRequest(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException { 
+    response.setContentType("text/html");
+    PrintWriter out = response.getWriter();
+    Utility util = new Utility();
+    // Récupère l'URL demandée
+    String url = request.getRequestURL().toString();
+    out.println(util.splitUrl(url));
+  }
+
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException { 
+    processRequest(request,response);
+  }  
+  
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException { 
+    processRequest(request,response);
+  }
+}
