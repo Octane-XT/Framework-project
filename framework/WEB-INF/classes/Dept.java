3f641f7d60be31434a7a8673d30a66d131ec319f
Octane-XT <zorakotoasimbola@hotmail.com>
2023-07-04 00:14:51 +0300

    sprint9

---
+++ framework/WEB-INF/classes/Dept.java
@@ -0,0 +1,38 @@
+package model;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+import annotation.Urls;
+import view.ModelView;
+
+public class Dept {
+    String nom;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Dept() {
+    }
+
+    public Dept(String nom) {
+        this.nom = nom;
+    }
+
+    @Urls(values = "Dept-add")
+    public void add_Dept() {
+        ModelView mv = new ModelView();
+        mv.setView("empadd.jsp");
+    }
+
+    @Urls(values = "test")
+    public static void test() {
+
+    }
+}
